4a89368825fe6e16b2f7faad7e92512a0900e103
Saharat <saharatseerakoon@gmail.com>
2025-12-17 14:41:54 +0700

    Add database connection setup with GORM and SQLite
    Introduces ConnectDatabase function in config package to establish a SQLite database connection using GORM. Logs a fatal error if the connection fails.

---
+++ backend/internal/config/database.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func ConnectDatabase() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("approval.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Cannot connect database:", err)
+	}
+	return db
+}
